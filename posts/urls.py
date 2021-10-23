97dc1040afcfd3d1ab1c17f938ce6468b214d9d5
Artus U <artusrock@hotmail.com>
2021-10-23 10:56:34 +0100

    PostCreateView

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    path('posts/', views.PostListView.as_view(), name='post-list')
+    path('posts/', views.PostListView.as_view(), name='post-list'),
+    path('posts/create/', views.PostCreateView.as_view(), name='post-create'),
+    path('posts/<slug>/', views.PostDetailView.as_view(), name='post-detail'),
 
 ]
